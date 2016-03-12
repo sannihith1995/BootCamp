0d30364859c35cc4297b6f6027204cf2b71ce93e
sannihith1995 <sannihith.1995@gmail.com>
2016-03-12 09:39:19 +0530

    Update Array_Swap.c

--- Array_Swap.c
+++ Array_Swap.c
@@ -2,23 +2,20 @@ nt main()
 {
 	int n,i=0,j,k,temp,d=0,a,arr[10];
 	scanf("%d",&n);
-	while(n>0)
-	{
+	while(n>0){
 		a=n%10;
 		d=d*10+a;
 		n=n/10;
 	}
 	n=d;
-	while(n>0)
-	{
+	while(n>0){
 		a=n%10;
 		arr[i++]=a;
 		n=n/10;
 	}
 	if(i%2==0)
 	{
-		for(j=0,k=i/2;j<i/2;j++,k++)
-		{
+		for(j=0,k=i/2;j<i/2;j++,k++){
 			temp=arr[j];
 			arr[j]=arr[k];
 			arr[k]=temp;
@@ -26,14 +23,14 @@ nt main()
 	}
 	else
 	{
-		for(j=0,k=(i+1)/2;j<(i-1)/2;j++,k++)
-		{
+		for(j=0,k=(i+1)/2;j<(i-1)/2;j++,k++){
 			temp=arr[j];
 			arr[j]=arr[k];
 			arr[k]=temp;
 		}
 	}
-	for(j=0;j<i;j++)
+	for(j=0;j<i;j++){
 		printf("%d",arr[j]);
+	}
 	return 0;
 }
